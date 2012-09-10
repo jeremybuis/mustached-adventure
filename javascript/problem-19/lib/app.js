744c99b14ca190a2eba64947bbc8d43f1287c86b
Jeremy Buis <buis.jeremy@gmail.com>
2012-09-10 03:34:58 -0400

    adding soln for problem-19

---
+++ javascript/problem-19/lib/app.js
@@ -0,0 +1,73 @@
+
+var monthNames = [
+  'january',
+  'february',
+  'march',
+  'april',
+  'may',
+  'june',
+  'july',
+  'august',
+  'september',
+  'october',
+  'november',
+  'december'
+];
+
+var months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+var daysOfTheWeek = [
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+  'sunday'
+];
+
+function app() {
+  var days = 0;
+  var weekday = 0;
+
+  var numSundays = 0;
+
+  for (var year=1900; year<=2000; year++) {
+    for (var month=1; month<=12; month++) {
+
+      //to deal with leap years - TODO: rework this to be more readable
+      if (month === 2) {
+
+        if (year%4 === 0) {
+          if (year%100 !== 0) {
+            days = 29;
+          } else if (year%400 === 0) {
+            days = 29;
+          } else {
+            days = months[month-1];
+          }
+        } else {
+          days = months[month-1];
+        }
+
+      } else {
+        days = months[month-1];
+      }
+
+      for (var day=1; day<=days; day++) {
+
+        if (year > 1900 && day === 1 && daysOfTheWeek[weekday] === 'sunday') {
+          numSundays++;
+        }
+        
+        weekday = (weekday + 1) % 7;
+      }
+    }
+  }
+
+  return numSundays;
+}
+
+module.exports = app;
+
+console.log(app());
---
+++ javascript/problem-19/test/app-test.js
@@ -0,0 +1,11 @@
+var should = require('should');
+
+var app = require('../lib/app');
+
+describe('problem-19 tests', function() {
+
+  it('should exist', function() {
+    should.exist(app);
+  });
+
+});
