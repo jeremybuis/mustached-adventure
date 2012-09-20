3462c293573ed82493a2c9014ef8f3cd99263e7d
Jeremy Buis <buis.jeremy@gmail.com>
2012-09-19 21:28:53 -0400

    changing the alg around

--- javascript/problem-28/lib/app.js
+++ javascript/problem-28/lib/app.js
@@ -1,3 +1,57 @@
+//http://ideone.com/9iL1F
+
+function spiralSquare(A, x, y, side, value) {
+  var mx = x + side-1;
+  var my = y + side-1;
+
+  var i;
+
+  console.log('x: ' + x + ' y: ' + y + ' side: ' + side + ' value: ' + value);
+
+  for (i=1; i<side; ++i) {
+    A[my-i][x] = value;
+
+    value++;
+  }
+
+  for (i=1; i<side; ++i) {
+    A[y][x+i] = value;
+
+    value++;
+  }
+
+  for (i=1; i<side; ++i) {
+    A[y+i][mx] = value;
+
+    value++;
+  }
+
+  for (i=1; i<side; ++i) {
+    A[my][mx-i] = value;
+
+    value++;
+  }
+
+  return value;
+}
+
+/*
+
+void spiral_square(Array& a, int x, int y, int side, int& value)
+{
+  int mx = x+side-1, my=y+side-1;
+  for (int i = 1; i <= side-1; ++i) a[my-i][x] = value++;
+  for (int i = 1; i <= side-1; ++i) a[y][x+i] = value++;
+  for (int i = 1; i <= side-1; ++i) a[y+i][mx] = value++;
+  for (int i = 1; i <= side-1; ++i) a[my][mx-i] = value++;
+}
+  
+for (int side=4;--middle>=0;side += 2) {
+  spiral_square(spiral, middle, middle, side, val);
+}
+
+*/
+
 //nxn spiral
 function generateSpiral(n) {
   var spiral = new Array(n);
@@ -9,63 +63,19 @@ function generateSpiral(n) {
     }
   }
 
-  var numItems = n * n;
+  //var numItems = n * n;
+  var value = 1;
+  var middle = 2;
 
-  var count = 1;
+  spiral[middle][middle] = value++;
+  spiral[middle][middle+1] = value++;
+  spiral[middle+1][middle+1] = value++;
+  spiral[middle+1][middle] = value++;
 
-  var currentDirection = 'up';
+  for (var side=4; --middle>=0; side += 2) {
+    value = spiralSquare(spiral, middle, middle, side, value);
 
-  var rowRight = 0;
-  var columnDown = n-1;
-  var rowLeft = n-1;
-  var columnUp = 0;
-
-  var row = 0;
-  var col = 0;
-
-  while (count <= numItems) {
-
-    if (currentDirection === 'up') {
-      row = rowRight++;
-
-      for (col=0; col<n; col++) {
-        if (spiral[row][col] === 0) {
-          spiral[row][col] = count++;
-        }
-      }
-
-      currentDirection = 'right';
-    } else if (currentDirection === 'right') {
-      col = columnDown--;
-
-      for (row=0; row<n; row++) {
-        if (spiral[row][col] === 0) {
-          spiral[row][col] = count++;
-        }
-      }
-
-      currentDirection = 'down';
-    } else if (currentDirection === 'down') {
-      row = rowLeft--;
-
-      for (col=n-1; col>=0; col--) {
-        if (spiral[row][col] === 0) {
-          spiral[row][col] = count++;
-        }
-      }
-
-      currentDirection = 'left';
-    } else if (currentDirection === 'left') {
-      col = columnUp++;
-
-      for (row=n-1; row>=0; row--) {
-        if (spiral[row][col] === 0) {
-          spiral[row][col] = count++;
-        }
-      }
-
-      currentDirection = 'up';
-    }
+    console.log(spiral);
   }
 
   return spiral;
